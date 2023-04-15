05b65b014b36fa552da237549f122978212d58da
k8snirmal <116304028+k8snirmal@users.noreply.github.com>
2023-04-15 13:41:34 +0530

    Update main.tf

--- demo-tf-state/main.tf
+++ demo-tf-state/main.tf
@@ -7,3 +7,13 @@ resource "aws_vpc" "demo_vpc" {
     Name    = "${var.candidate_name}-vpc"
   }
 }
+
+#create subnet
+resource "aws_subnet" "public-1" {
+  vpc_id     = aws_vpc.demo_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
